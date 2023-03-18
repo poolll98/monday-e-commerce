756924ede533a67d699a8c0c90e4bf485e2f48cd
rafb <raffael.botschen@zuehlke.com>
2023-03-18 15:26:35 +0100

    More styling examples.

--- Frontend/src/Components/HeaderBar.js
+++ Frontend/src/Components/HeaderBar.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function HeaderBar() {
+    const colors = {'color': 'blue'}
     return (
-        <div style={{'textAlign': "center"}}>
+        <div style={{...colors, 'textAlign': "center"}}>
             <Link to="/">MarketMate</Link>
             <h3>Here be the header</h3>
         </ div>);
