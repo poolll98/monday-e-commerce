acec00df086d03b3f929a48a3a1919c8d5d73e67
rafb <raffael.botschen@zuehlke.com>
2023-03-18 15:22:37 +0100

    Added example for inline css.

--- Frontend/src/Components/HeaderBar.js
+++ Frontend/src/Components/HeaderBar.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 
 export default function HeaderBar() {
     return (
-        <>
+        <div style={{'textAlign': "center"}}>
             <Link to="/">MarketMate</Link>
             <h3>Here be the header</h3>
-        </>);
+        </ div>);
 }
