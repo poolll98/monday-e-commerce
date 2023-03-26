6a92719d66e0358462602683757b44ec7b37ad70
Huijiao Luo <huijiao.luo@uzh.ch>
2023-03-26 23:45:14 +0800

    list all products

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,27 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import "./App.css";
-import HeaderBar from "./components/HeaderBar";
-import ProductPage from "./components/ProductPage/ProductPage";
-import HomePage from "./components/HomePage";
-import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
-import { UserProvider } from "./components/UserContext";
+import './App.css';
+import HeaderBar from './components/HeaderBar';
+import ProductPage from './components/ProductPage/ListProducts';
+import ProductDetail from './components/ProductPage/ProductPage';
+import HomePage from './components/HomePage';
+import ShoppingCart from './components/ShoppingCart/ShoppingCart';
+import { UserProvider } from './components/UserContext';
+
 
 function App() {
+
   return (
     <div className="wrapper">
       <BrowserRouter>
         <UserProvider>
-          <HeaderBar />
+            <HeaderBar />
           <Routes>
             <Route exact path="/" element={<HomePage />} />
             <Route exact path="/cart" element={<ShoppingCart />} />
-            <Route path="/products/:productId" element={<ProductPage />} />
+            <Route exact path="/products" element={<ProductPage />} />
+            <Route path="/products/:productId" element={<ProductDetail />} />
           </Routes>
         </UserProvider>
       </BrowserRouter>
     </div>
   );
 }
+
 
 export default App;
