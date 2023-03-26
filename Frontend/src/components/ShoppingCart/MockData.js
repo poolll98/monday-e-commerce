64a0373a5293c27975f1b26821de54e66993890e
rafb <raffael.botschen@zuehlke.com>
2023-03-26 20:59:24 +0200

    Formatting, fix img links.

--- Frontend/src/components/ShoppingCart/MockData.js
+++ Frontend/src/components/ShoppingCart/MockData.js
@@ -1,27 +1,27 @@
-import Shoes1 from './images/Shoes1.jpg'
-import Shoes2 from './images/Shoes2.jpg'
-import Shoes3 from './images/Shoes3.jpg'
+import Shoes1 from "./images/Shoes1.jpg";
+import Shoes2 from "./images/Shoes2.jpg";
+import Shoes3 from "./images/Shoes3.jpg";
 
 export default [
-    {
-        "name":"Shoes1",
-        "img":Shoes1,
-        "price":12.34,
-        "count":5,
-        "selected":true
-    },
-    {
-        "name":"Shoes2",
-        "img":Shoes2,
-        "price":56.78,
-        "count":4,
-        "selected":true
-    },
-    {
-        "name":"Shoes3",
-        "img":Shoes3,
-        "price":90.12,
-        "count":3,
-        "selected":false
-    }
-  ];
+  {
+    name: "Shoes1",
+    img: Shoes1,
+    price: 12.34,
+    count: 5,
+    selected: true,
+  },
+  {
+    name: "Shoes2",
+    img: Shoes2,
+    price: 56.78,
+    count: 4,
+    selected: true,
+  },
+  {
+    name: "Shoes3",
+    img: Shoes3,
+    price: 90.12,
+    count: 3,
+    selected: false,
+  },
+];
--- Frontend/src/components/ShoppingCart/ShoppingCart.js
+++ Frontend/src/components/ShoppingCart/ShoppingCart.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { useContext } from 'react';
-import { useState, useEffect, useCallback } from 'react';
-import { UserContext } from '../UserContext';
+import React from "react";
+import { useContext } from "react";
+import { useState, useEffect, useCallback } from "react";
+import { UserContext } from "../UserContext";
 
-import CartItem from './CartItem';
+import CartItem from "./CartItem";
 
-import './ShoppingCart.css'
-import productData from "./MockData";
+import "./ShoppingCart.css";
+import productData from "./MockData.js";
 
 export default function ShoppingCart() {
   //let items = [{'id': 1}, {'id': 2}, {'id': 3}]; // example items
@@ -16,106 +16,91 @@ export default function ShoppingCart() {
 
   //let cartItems = items.map(item => <CartItem item={item} key={item.id} isActive={activeIndex === item.id} onHighlight={() => setActiveIndex(item.id)}/>)
 
-
-  const [cart, setCart] = useState([])
-  const [allSelected, setAllSelected] = useState(false)
+  const [cart, setCart] = useState([]);
+  const [allSelected, setAllSelected] = useState(false);
 
   /* Add item */
   const addItem = useCallback(
-      (i) => {
-          console.log("addItem", i);
-          setCart(cart.map(
-              (item, _i) => {
-                  _i === i && item.count++
-                  return item
-              }
-          ))
-      },
-      [cart],
-  )
+    (i) => {
+      console.log("addItem", i);
+      setCart(
+        cart.map((item, _i) => {
+          _i === i && item.count++;
+          return item;
+        })
+      );
+    },
+    [cart]
+  );
 
   /* reduce item */
   const subItem = useCallback(
-      (i) => {
-          console.log("subItem", i);
-          setCart(cart.map(
-              (item, _i) => {
-                  (_i === i && item.count) && item.count--
-                  return item
-              }
-          ))
-      },
-      [cart],
-  )
+    (i) => {
+      console.log("subItem", i);
+      setCart(
+        cart.map((item, _i) => {
+          _i === i && item.count && item.count--;
+          return item;
+        })
+      );
+    },
+    [cart]
+  );
 
   /* remove item */
   const removeItem = useCallback(
-      (i) => {
-          console.log("removeItem", i);
-          setCart(cart.filter((item, _i) => _i !== i))
-      },
-      [cart],
-  )
+    (i) => {
+      console.log("removeItem", i);
+      setCart(cart.filter((item, _i) => _i !== i));
+    },
+    [cart]
+  );
 
   /* Select and unselect item */
   const toggleItem = useCallback(
-      (i) => {
-          console.log("toggleItem", i);
-          console.log("cart", cart);
+    (i) => {
+      console.log("toggleItem", i);
+      console.log("cart", cart);
 
-          const item = cart.find(
-              (item, _i) => {
-                  console.log("_i", _i);
-                  return _i === i
-              }
-          )
+      const item = cart.find((item, _i) => {
+        console.log("_i", _i);
+        return _i === i;
+      });
 
-          if (item) {
-              console.log("here");
-              item.selected = !item.selected
-              setCart([...cart])
-          }
-      },
-      [cart],
-  )
+      if (item) {
+        console.log("here");
+        item.selected = !item.selected;
+        setCart([...cart]);
+      }
+    },
+    [cart]
+  );
 
   /* Calculate total quantity */
-  const getTotalCount = useCallback(
-      () => {
-          // console.log("getTotalItems");
-          return cart.filter(
-              item => item.selected
-          ).reduce(
-              (pv, cv, i) => {
-                  // console.log(pv, cv);
-                  return pv + cv.count
-              },
-              0
-          )
-      },
-      [cart],
-  )
+  const getTotalCount = useCallback(() => {
+    // console.log("getTotalItems");
+    return cart
+      .filter((item) => item.selected)
+      .reduce((pv, cv, i) => {
+        // console.log(pv, cv);
+        return pv + cv.count;
+      }, 0);
+  }, [cart]);
 
   /* calculate total amount */
-  const getTotalAmount = useCallback(
-      () => {
-          // console.log("getTotalAmount");
-          return cart.filter(
-              item => item.selected
-          ).reduce(
-              (pv, cv, i) => {
-                  // console.log(pv, cv);
-                  return pv + cv.price * cv.count
-              },
-              0
-          )
-      },
-      [cart],
-  )
+  const getTotalAmount = useCallback(() => {
+    // console.log("getTotalAmount");
+    return cart
+      .filter((item) => item.selected)
+      .reduce((pv, cv, i) => {
+        // console.log(pv, cv);
+        return pv + cv.price * cv.count;
+      }, 0);
+  }, [cart]);
 
   useEffect(() => {
-      !cart.length && setCart(productData)
-  }, [])
+    !cart.length && setCart(productData);
+  }, [cart]);
 
   /* ===== Select all -- "two-way data binding" ===== */
   /* 
@@ -123,90 +108,86 @@ export default function ShoppingCart() {
    cart driver allSelected
   */
   useEffect(() => {
-      // console.log("useEffect", "cart=>allSelected");
-      cart.every(item => item.selected)
-          ? setAllSelected(true)
-          : setAllSelected(false)
-      // return () => { }
-  }, [cart])
-
+    // console.log("useEffect", "cart=>allSelected");
+    cart.every((item) => item.selected)
+      ? setAllSelected(true)
+      : setAllSelected(false);
+    // return () => { }
+  }, [cart]);
 
   /*     
   When the user actively uses the [Select All] function
   First modify the cart data (select all or delete all) and then drive allSelected by cart 
   */
   const toggleAllSelected = useCallback(
-      (value) => {
-          console.log("toggleAllSelected", value);
-          // setAllSelected(value)
+    (value) => {
+      console.log("toggleAllSelected", value);
+      // setAllSelected(value)
 
-          // First modify the cart data to (select all or delete all)
-          // Modify shopping cart data => [data listening logic A] will trigger setAllSelected
-          cart.forEach(item => item.selected = value)
-          setCart([...cart])
-      },
-      [cart],
-  )
+      // First modify the cart data to (select all or delete all)
+      // Modify shopping cart data => [data listening logic A] will trigger setAllSelected
+      cart.forEach((item) => (item.selected = value));
+      setCart([...cart]);
+    },
+    [cart]
+  );
   /* ===== ENDOF Select all -- "two-way data binding" ===== */
 
   /* functional component => render JSX */
   return (
-      <div className="cart-wrapper">
+    <div className="cart-wrapper">
+      <div className="top">
+        <div className="sel-box">
+          <input
+            type="checkbox"
+            checked={allSelected}
+            onChange={(e) => {
+              console.log("SelectAll input onChanged", !allSelected);
+              toggleAllSelected(!allSelected);
+            }}
+          />
+          <i>SelectAll</i>
+        </div>
 
-          <div className="top">
-              <div className='sel-box'>
-                  <input type="checkbox"
-                      checked={allSelected}
-                      onChange={
-                          (e) => {
-                              console.log("SelectAll input onChanged", !allSelected);
-                              toggleAllSelected(!allSelected)
-                          }
-                      } />
-                  <i>SelectAll</i>
-              </div>
-
-              <span className='imgname-box'>Product Name</span>
-              <span>Unit Price</span>
-              <span className='count-box'>Quantity</span>
-              <span>Amount</span>
-              <span>Operation</span>
-          </div>
-
-          <div className="middle">
-              <ul>{
-                  cart.map(
-                      (item, i) =>
-                          <CartItem
-                              index={i}
-                              item={item}
-                              addItem={addItem}
-                              subItem={subItem}
-                              removeItem={removeItem}
-                              toggleSelection={toggleItem}
-                              key={item.name}>
-                          </CartItem>
-                  )
-              }</ul>
-          </div>
-
-          <div className="bottom">
-              <div className="left"></div>
-              <div className='count-box'>
-                  <span className='price'>{getTotalCount()}</span> items has been selected
-              </div>
-              <div className='amount-box'>
-                  Total Amount <span className='price'>{getTotalAmount().toFixed(2)}</span>
-              </div>
-              <div className="pay-box">
-                  <i>Buy</i>
-              </div>
-          </div>
-
+        <span className="imgname-box">Product Name</span>
+        <span>Unit Price</span>
+        <span className="count-box">Quantity</span>
+        <span>Amount</span>
+        <span>Operation</span>
       </div>
+
+      <div className="middle">
+        <ul>
+          {cart.map((item, i) => (
+            <CartItem
+              index={i}
+              item={item}
+              addItem={addItem}
+              subItem={subItem}
+              removeItem={removeItem}
+              toggleSelection={toggleItem}
+              key={item.name}
+            ></CartItem>
+          ))}
+        </ul>
+      </div>
+
+      <div className="bottom">
+        <div className="left"></div>
+        <div className="count-box">
+          <span className="price">{getTotalCount()}</span> items has been
+          selected
+        </div>
+        <div className="amount-box">
+          Total Amount{" "}
+          <span className="price">{getTotalAmount().toFixed(2)}</span>
+        </div>
+        <div className="pay-box">
+          <i>Buy</i>
+        </div>
+      </div>
+    </div>
   );
-
-
 
   //return (user.username !== undefined && <ul>{cartItems}</ul>);
 }
