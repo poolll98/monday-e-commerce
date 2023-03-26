00a58ba70c13aa4aa5dcc8b5c326e78950fe0f26
rafb <raffael.botschen@zuehlke.com>
2023-03-26 14:39:08 +0200

    Fix routing, formatting.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,32 +1,27 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './App.css';
-import HeaderBar from './components/HeaderBar';
-import ProductPage from './components/ProductPage/ProductPage';
-import ProductDetail from './components/ProductPage/ProductDetails';
-import HomePage from './components/HomePage';
-import ShoppingCart from './components/ShoppingCart/ShoppingCart';
-import { UserProvider } from './components/UserContext';
-
+import "./App.css";
+import HeaderBar from "./components/HeaderBar";
+import ProductPage from "./components/ProductPage/ProductPage";
+import HomePage from "./components/HomePage";
+import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
+import { UserProvider } from "./components/UserContext";
 
 function App() {
-
   return (
     <div className="wrapper">
       <BrowserRouter>
         <UserProvider>
-            <HeaderBar />
+          <HeaderBar />
           <Routes>
             <Route exact path="/" element={<HomePage />} />
             <Route exact path="/cart" element={<ShoppingCart />} />
-            <Route exact path="/products" element={<ProductPage />} />
-            <Route path="/products/:productId" element={<ProductDetail />} />
+            <Route path="/products/:productId" element={<ProductPage />} />
           </Routes>
         </UserProvider>
       </BrowserRouter>
     </div>
   );
 }
-
 
 export default App;
--- Frontend/src/components/HeaderBar.js
+++ Frontend/src/components/HeaderBar.js
@@ -1,19 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { UserContext, UserLoginContext } from './UserContext';
-import { useContext } from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import { UserContext, UserLoginContext } from "./UserContext";
+import { useContext } from "react";
 
 export default function HeaderBar() {
-    const colors = {'color': 'blue'}
+  const colors = { color: "blue" };
 
-    const user = useContext(UserContext);
-    const userAction = useContext(UserLoginContext);
+  const user = useContext(UserContext);
+  const userAction = useContext(UserLoginContext);
 
-    return (
-        <div style={{...colors, 'textAlign': "center"}}>
-            <Link to="/">MarketMate</Link>
-            <h3>Here be the header</h3>
-            {(user.username === undefined && <button onClick={() => {userAction({type: 'login'});}}>Log In</button>)}
-            {(user.username !== undefined && <button onClick={() => {userAction({type: 'logout'})}}>Log Out</button>)}
-        </ div>);
+  return (
+    <div style={{ ...colors, textAlign: "center" }}>
+      <Link to="/">MarketMate</Link>
+      <h3>Here be the header</h3>
+      {user.username === undefined && (
+        <button
+          onClick={() => {
+            userAction({ type: "login" });
+          }}
+        >
+          Log In
+        </button>
+      )}
+      {user.username !== undefined && (
+        <button
+          onClick={() => {
+            userAction({ type: "logout" });
+          }}
+        >
+          Log Out
+        </button>
+      )}
+      <Link to="/cart">Shopping Cart</Link>
+    </div>
+  );
 }
