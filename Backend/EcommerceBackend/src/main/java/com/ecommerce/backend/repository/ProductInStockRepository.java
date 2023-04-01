cb840b2e986afb91661e677bd1958282bd485b91
raul-doru.m <raul-doru.mateoc@uzh.ch>
2023-04-01 23:05:47 +0200

    instock

--- Backend/EcommerceBackend/src/main/java/com/ecommerce/backend/repository/ProductInStockRepository.java
+++ Backend/EcommerceBackend/src/main/java/com/ecommerce/backend/repository/ProductInStockRepository.java
@@ -1,8 +1,6 @@
 package com.ecommerce.backend.repository;
 import java.util.List;
-import java.util.Locale;
 import java.util.Optional;
-import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,9 +11,9 @@ import com.ecommerce.backend.models.Product;
 
 @Repository
 public interface ProductInStockRepository extends JpaRepository<Product, Boolean>{
-    Optional<Product> findByInStock(Boolean inStock);
+    Optional<Product> findByInStock(Boolean instock);
     @Query(value = "SELECT * FROM public.product WHERE instock=:instock", nativeQuery = true)
-    List<Product> findProductByStock(@Param("instock") Boolean inStock);
+    List<Product> findProductByStock(@Param("instock") Boolean instock);
 }
 
 
