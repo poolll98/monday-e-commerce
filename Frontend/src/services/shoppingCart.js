7fcc4b9ce17a8b127736f9688ed2b2bc276a97f6
rafb <raffael.botschen@zuehlke.com>
2023-04-01 20:13:55 +0200

    Mock data loading.

--- Frontend/src/services/shoppingCart.js
+++ Frontend/src/services/shoppingCart.js
@@ -1,4 +1,7 @@
 // TODO: Check if user needs to be passed or can be extracted from context.
+// TODO: Connection error handling.
+
+import products from "../mockdata/products";
 
 const cartEndpointUrl = "http://localhost:3333/cart";
 
@@ -7,7 +10,15 @@ export function getShoppingCartItems(user) {
     // do something?
   }
 
-  return fetch(cartEndpointUrl).then((data) => data.json());
+  return fetch(cartEndpointUrl)
+    .then((data) => data.json())
+    .then((items) => {
+      // TODO: Load actual data.
+      let f = items
+        .map((item) => products.find((product) => product.id === item.id))
+        .filter((product) => product !== undefined);
+      return f;
+    });
 }
 
 export function addItemToCart(itemData, user) {
@@ -27,7 +38,10 @@ export function addItemToCart(itemData, user) {
   }).then((response) => {
     if (!response.ok) {
       alert(`Error when adding item to cart.`);
-      console.log(`Could not store item ${body} in cart: ${response}`);
+      console.log(`Could not store item ${body} in cart.`);
+      console.log(response);
+    } else {
+      alert("success");
     }
     // Add item to header shopping cart indicator?
   });
