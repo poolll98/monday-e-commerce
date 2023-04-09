87efc5bfef495542cdbbaecb75856303181d007c
rafb <raffael.botschen@zuehlke.com>
2023-04-09 12:09:10 +0200

    Added my first tests.

---
+++ Frontend/src/components/HeaderBar.test.js
@@ -0,0 +1,39 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import HeaderBar from "./HeaderBar";
+import App from "../App";
+const { Router } = require("react-router-dom");
+
+const { createMemoryHistory } = require("history");
+
+test("renders the log in button in the header bar when not logged in", () => {
+  render(<App />);
+  expect(screen.getByRole("button", { name: "Log In" })).toBeInTheDocument();
+});
+
+test("renders the log out button in the header bar when logged in", () => {
+  localStorage.setItem("bearerToken", "test");
+  render(<App />);
+  expect(screen.getByRole("button", { name: "Log Out" })).toBeInTheDocument();
+});
+
+test("clicking the MarketMate link returns to the homepage", () => {
+  const history = createMemoryHistory();
+  render(
+    <Router location={history.location} navigator={history}>
+      <HeaderBar />
+    </Router>
+  );
+  fireEvent.click(screen.getByText("MarketMate"));
+  expect(history.location.pathname).toBe("/");
+});
+
+test("clicking the cart link routes to the shopping cart", () => {
+  const history = createMemoryHistory();
+  render(
+    <Router location={history.location} navigator={history}>
+      <HeaderBar />
+    </Router>
+  );
+  fireEvent.click(screen.getByText("Shopping Cart"));
+  expect(history.location.pathname).toBe("/cart");
+});
