fad968d0ec514efd75951ed1254d1a99022abbac
Huijiao Luo <huijiao.luo@uzh.ch>
2023-04-15 06:41:57 +0800

    modify App.js

--- Frontend/src/components/SearchBar.js
+++ Frontend/src/components/SearchBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from 'react';
-import productData from "../../mockdata/products";
+import productData from "../mockdata/products";
 import { Link } from "react-router-dom";
 
 function SearchBar({ query, onChange }) {
