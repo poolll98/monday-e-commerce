d541373a9d0e34cca725f090cb0f6454538070e6
rafb <raffael.botschen@zuehlke.com>
2023-04-16 15:55:08 +0200

    Make mock request.

--- Frontend/src/services/search.js
+++ Frontend/src/services/search.js
@@ -1,16 +1,5 @@
-import searchProducts from "../mocks/searchProducts";
-
-const searchEndpointUrl = "http://localhost:3333/search/name/";
+const searchEndpointUrl = "http://localhost:8080/search/name/";
 
 export function searchItems(searchTerm) {
-  searchTerm = searchTerm.toLowerCase();
-
-  if (searchTerm.trim().length === 0) {
-    return searchProducts;
-  }
-  return searchProducts.filter((product) =>
-    product.name.toLowerCase().includes(searchTerm)
-  );
-
   return fetch(searchEndpointUrl + searchTerm).then((data) => data.json());
 }
