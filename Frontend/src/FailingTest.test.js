70b25cf244015144b1fd6c26b6634d843da91e0e
rafb <raffael.botschen@zuehlke.com>
2023-04-29 19:12:50 +0200

    Remove failing test, run only on develop, master.

--- Frontend/src/FailingTest.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders the log in button in the header bar when not logged in", () => {
-  render(<App />);
-  expect(
-    screen.getByRole("button", { name: "Not Existing" })
-  ).toBeInTheDocument();
-});
