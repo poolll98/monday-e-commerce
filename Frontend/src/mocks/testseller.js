dfe374be1b75da8bf900f55ca9e7dcc6aae06ffa
Ruijie_Wang <ruijie.wang2@uzh.ch>
2023-04-30 14:43:20 +0200

    The seller profile function is added on the basis of the user profile, and the seller can view account information and product list.

---
+++ Frontend/src/mocks/testseller.js
@@ -0,0 +1,10 @@
+export default {
+    username: "test123",
+    email: "test123@gmail.com",
+    password: "testpassw",
+    phone: "3456787981",
+    firstname: "test_name",
+    lastname: "test_surname",
+    address: "Dietlikon 8305",
+    isseller: true,
+  };
